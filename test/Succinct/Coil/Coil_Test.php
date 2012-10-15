739896bb7685e73e9589a71135e22878083f6cae
Darian Brown <darianbr@gmail.com>
2012-10-15 14:57:34 +1000

    Added phpunit tests

---
+++ test/Succinct/Coil/Coil_Test.php
@@ -0,0 +1,13 @@
+<?php
+
+use Succinct\Coil\Coil;
+
+class Coil_Test extends PHPUnit_Framework_TestCase {
+
+	public function testTFW() {
+		$db = new Coil();
+		$this->assertEquals(2,1);
+
+	}
+
+}
